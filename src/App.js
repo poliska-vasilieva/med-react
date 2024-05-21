94d5762206f531e6ca8f023a2ff2a5462336d3ff
polina <polvasilieva@list.ru>
2024-05-21 19:17:46 +0500

    img + block with news

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
-import logo from "./logo.svg";
+import logo from "./img/Logo 2.png";
+import mainNewPicture from "./img/pic2 1.png";
+import oneSmallPict from "./img/pic2 1 (1).png";
+import twoSmallPict from "./img/pic2 2.png";
+import threeSmallPict from "./img/pic2 3.png";
 import React, { Component } from 'react';
 
 class Navbar extends Component {
@@ -10,8 +14,8 @@ class Navbar extends Component {
     const nameFofth = 'Научная деятельность';
 
     return <div className='Nav'>
+      <div><img src={logo} className="App-logo" alt="logo" /></div>
       <ul>
-        <li><img src={logo} className="App-logo" alt="logo" /></li>
         <li>{nameFirst}</li>
         <li>{nameSecond}</li>
         <li>{nameThird}</li>
@@ -27,12 +31,43 @@ class BlockNews extends Component {
 
     return <div className='mainBlock'>
       <div className='firstBlock'>
-        <div className='imgfirstBlock'></div>
+        <div><img src={mainNewPicture} className="mainNewPicture" alt="mainNewPicture" /></div>
+        <p>3 октября 2023</p>
+        <p>Второй этап конкурса на звание «Лучший врач педиатр участковый»</p>
+        <p>3 октября 2023 года прошло тестирование практических навыков врачей педиатров участковых в обучающем...</p>
+        <p>{link}</p>
+      </div>
 
-      </div>
       <div className='secondBlock'>
-        <h1>{link}</h1>
+        <div className='threeNews'>
+          <div className='oneNew'>
+            <div><img src={oneSmallPict} className="oneSmallPict" alt="oneSmallPict" /></div>
+            <p>26 сентября 2023</p>
+            <p>Международные студенческие обмены. Опыт и перспективы</p>
+          </div>
+        </div>
+
+        <div className='threeNews'>
+          <div className='twoNew'>
+            <div><img src={twoSmallPict} className="twoSmallPict" alt="twoSmallPict" /></div>
+            <p>16 сентября 2023</p>
+            <p>Встреча выпускников 1983 года</p>
+          </div>
+        </div>
+
+        <div className='threeNews'>
+          <div className='threeNew'>
+            <div><img src={threeSmallPict} className="threeSmallPict" alt="threeSmallPict" /></div>
+            <p>14 сентября 2023</p>
+            <p>Противодействие идеологии терроризма и профилактики экстремизма в социальных сетях</p>
+          </div>
+        </div>
       </div>
+
+      <div className='thirdBlock'>
+        <p>здесь будет меню с подразделениями Университета</p>
+      </div>
+
     </div>
   }
 }
@@ -40,8 +75,10 @@ class BlockNews extends Component {
 function App() {
   return (
     <div className='App'>
-      <Navbar />,
-      <BlockNews />
+      <div className='container'>
+        <Navbar />
+        <BlockNews />
+      </div>
     </div>
   );
 }
